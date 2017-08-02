6c6ae678bc5934c336792f5a804e8a9063339d80
yhl221 <1727538058@qq.com>
2017-08-02 20:08:22 +0800

    commit component home.js

---
+++ public/js/component/home.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+
+class Home extends Component{
+
+    componentWillMount(){
+        this.props.getAllUser();
+    }
+
+
+    render(){
+        return <div>
+            <div className="header">
+                <h1>用户信息管理</h1>
+            </div>
+        </div>
+    }
+}
+
+export default Home;
