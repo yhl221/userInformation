2cdf0faf215bcb552a85616ebc8f0c37169e2477
yhl221 <1727538058@qq.com>
2017-08-03 19:33:55 +0800

    routers response userInformation

---
+++ server/dbs/searchUser.js
@@ -0,0 +1,16 @@
+
+let db = require('../helps/get-urls');
+let userSQL = require('../helps/userSql');
+
+function searchUser(userID,callback) {
+    db.query(userSQL.queryById, userID, function (err, result) {
+        if (err) {
+            return err;
+        }
+        else {
+            callback(result);
+        }
+    });
+}
+module.exports = searchUser;
+
--- server/routers/searchUser.js
+++ server/routers/searchUser.js
@@ -1,8 +1,14 @@
 const express=require("express");
 const router=express.Router();
+const searchUser=require('../dbs/searchUser');
 
 router.post('/searchUser',(req,res)=>{
     console.log(req.body)
+    let ID=req.body.userID;
+    searchUser(ID,(result)=>{
+        console.log(result);
+        res.send({information:result});
+    })
 });
 
 module.exports=router;
