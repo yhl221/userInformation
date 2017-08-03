5296765246f56269053d69620f0e325e168452b0
yhl221 <1727538058@qq.com>
2017-08-03 14:27:33 +0800

    modify can get the information of user

--- public/js/component/modify.js
+++ public/js/component/modify.js
@@ -2,36 +2,44 @@ import React, {Component} from "react";
 
 class Modify extends Component{
 
-    componentWillMount(){
-        console.log("this.state",this.props.oneUser);
-    }
+    componentDidMount(){
+            var information=this.props.oneUser;
+            document.getElementById("userName").value=information.user_name;
+            document.getElementById("name").value=information.name;
+            document.getElementById("age").value=information.age;
+            document.getElementById("sex").value=information.sex;
+            document.getElementById("phone").value=information.phone;
+            document.getElementById("email").value=information.email;
+            document.getElementById("mark").value=information.mark;
+        }
 
     render(){
+
         return <div>
             <div className="header">
                 <h1>修改用户信息</h1>
             </div>
             <div>
                 <div>
-                    <input type="text" className="inputStyle" placeholder="用户名" id="userName"/>
+                    <input type="text" className="inputStyle" id="userName"placeholder="用户名"/>
                 </div>
                 <div>
-                    <input type="text" className="inputStyle" placeholder="姓名" id="name"/>
+                    <input type="text" className="inputStyle"  id="name"/>
                 </div>
                 <div>
-                    <input type="text" className="inputStyle" placeholder="年龄" id="age"/>
+                    <input type="text" className="inputStyle"  id="age"/>
                 </div>
                 <div>
-                    <input type="text" className="inputStyle" placeholder="性别" id="sex"/>
+                    <input type="text" className="inputStyle"  id="sex"/>
                 </div>
                 <div>
-                    <input type="text" className="inputStyle" placeholder="移动电话" id="phone"/>
+                    <input type="text" className="inputStyle"  id="phone"/>
                 </div>
                 <div>
-                    <input type="text" className="inputStyle" placeholder="电子邮件" id="email"/>
+                    <input type="text" className="inputStyle"  id="email"/>
                 </div>
                 <div>
-                    <input type="text" className="inputStyle" placeholder="备注" id="mark"/>
+                    <input type="text" className="inputStyle"  id="mark"/>
                 </div>
                 <dib>
                     <button className="btn btn-info btnStyle">提交信息</button>
