2d20b47410939ef121e94f70e596edc7038eaab7
yhl221 <1727538058@qq.com>
2017-08-03 12:33:16 +0800

    rename getAll to home

--- public/js/reducer/home.js
+++ public/js/reducer/home.js
@@ -1,8 +1,10 @@
-module.exports=((state={users:[]},action)=>{
+module.exports=((state={users:[],oneuser:[]},action)=>{
     console.log("reducer",action.users );
     switch(action.type){
         case 'GETALLUSER':
             return ({users:action.users});
+        case "FINDONE":
+            return ({onUser:action.data});
         default:
             return state;
     }
