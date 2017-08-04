ad128d71edd5ece9741f205bea2f6fa4df14e158
yhl221 <1727538058@qq.com>
2017-08-04 17:15:17 +0800

    modify add user

--- spec/apis/add-test.js
+++ spec/apis/add-test.js
@@ -3,6 +3,7 @@ const server = require('../../server');
 
 describe('server', ()=> {
     it('return success to add user', (done)=> {
+        
         request(server)
             .post('/insertUser')
             .send({user_name: "wsf", name: "fg", age: 34, sex: "fsdf", phone: "fgvd", email: "dbf", mark: "bfg"})
