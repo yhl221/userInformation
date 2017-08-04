2ac7d5c451d2bf28f2c2fb9d28d3212e649c26cd
yhl221 <1727538058@qq.com>
2017-08-04 10:25:49 +0800

    finish test of addUser reducer

--- spec/reducers/add-test.js
+++ spec/reducers/add-test.js
@@ -1,10 +1,16 @@
 const addUser=require('../../public/js/reducer/add');
 const  expect=require('chai').expect;
 
-describe('/addUserReducer',()=>{
-    it('save userInformation with "ADDUSER"',()=>{
-        const state={isSave:false};
-        const action={type:"ADDUSER",isSaved:true};
-        expect(addUser(state,action)).to.be.deep.equal({isSaved:true});
+describe('/addUserReducer', ()=> {
+    it('save userInformation with "ADDUSER"', ()=> {
+        const state = {isSave: false};
+        const action = {type: "ADDUSER", isSaved: true};
+        expect(addUser(state, action)).to.be.deep.equal({isSaved: true});
+    });
+
+    it('reset the state of isSaved', function () {
+        const state = {isSaved: false};
+        const action = {type: "RESET", isSaved: false};
+        expect(addUser(state, action)).to.be.deep.equal({isSaved: false});
     })
 });
