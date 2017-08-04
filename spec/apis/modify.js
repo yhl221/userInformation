0298783dafcbf90362147ff0667c5eaac73380df
yhl221 <1727538058@qq.com>
2017-08-04 17:12:05 +0800

    modify

--- spec/apis/modify.js
+++ spec/apis/modify.js
@@ -6,7 +6,7 @@ describe('test the api of modify', ()=> {
         request(server)
             .post('/modifyUser')
             .send({
-                type: "MODIFYUSER", data: {
+                 data: {
                     age: 30, email: "dbf", mark: "bfg", name: "fg",
                     phone: "fgvd", sex: "fsdf", user_id: 51, user_name: "wsfghf"
                 }
